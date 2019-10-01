cbb0e1d48a1946e6fb68cd21a5614473c4a1ecfe
Cameron Moir <cameronjmoir@gmail.com>
2019-10-01 16:21:37 +0100

    final commit

--- controller.rb
+++ controller.rb
@@ -6,6 +6,10 @@ require_relative('models/game')
 also_reload('models/*')
 
 get "/game/:choice1/:choice2" do
-  @game = Game.rock_paper_scissors(params[:choice1].downcase, params[:choice2].downcase)
+  @game = Game.rock_paper_scissors(params[:choice1], params[:choice2])
   erb(:result)
+end
+
+get "/about_us" do
+  erb(:about_us)
 end
