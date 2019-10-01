1bbba9f769b49ef806c899ad25eaace68add4dce
Cameron Moir <cameronjmoir@gmail.com>
2019-10-01 16:05:25 +0100

    result of game prints to screen

--- controller.rb
+++ controller.rb
@@ -1,5 +1,11 @@
 require('sinatra')
 require('sinatra/contrib/all') if development?
 require('pry')
+
 require_relative('models/game')
 also_reload('models/*')
+
+get "/game/:choice1/:choice2" do
+  @game = Game.rock_paper_scissors(params[:choice1].downcase, params[:choice2].downcase)
+  erb(:result)
+end
